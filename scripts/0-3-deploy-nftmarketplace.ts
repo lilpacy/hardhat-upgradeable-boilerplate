25e55a6fe9561a0ad5d0eb38bba50f3f55e239cf
lilpacy <revivedtomorrow@gmail.com>
2023-10-19 13:16:07 +0900

    Deploy NFTMarketplace with proxy

--- scripts/0-3-deploy-nftmarketplace.ts
+++ scripts/0-3-deploy-nftmarketplace.ts
@@ -1,22 +1,31 @@
-import { ethers } from "hardhat";
-import { NFTMarketplace } from "../typechain"; // eslint-disable-line node/no-missing-import
+import { ethers, upgrades } from "hardhat";
+import { NFTMarketplace, NFTMarketplace__factory } from "../typechain"; // eslint-disable-line node/no-missing-import
 import { constants } from "../constants";
 
 async function main() {
-  const erc20 = constants.erc20Address;
-  const erc721 = constants.erc721Address;
+  const erc721Addr = constants.erc721Address;
+  const erc20Adrr = constants.erc20Address;
 
-  const Contract = await ethers.getContractFactory("NFTMarketplace");
-  const contract = await Contract.deploy(erc721, erc20);
+  const [owner] = await (ethers as any).getSigners();
+  const contract = (await upgrades.deployProxy(
+    new NFTMarketplace__factory(owner),
+    [erc721Addr, erc20Adrr],
+    { initializer: "initialize" },
+  )) as NFTMarketplace;
   await contract.deployed();
 
-  console.log(`Contract deployed to ${contract.address}`);
-  console.log(`NFTMarketplace set to ${erc721}, ${erc20}`);
+  console.log(`ProxyStorage: ${contract.address}`);
+  console.log(
+    "Implementation: ",
+    await upgrades.erc1967.getImplementationAddress(contract.address),
+  );
+  console.log("Admin: ", await upgrades.erc1967.getAdminAddress(contract.address));
+  console.log(`NFTMarketplace set to ${erc721Addr}, ${erc20Adrr}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
